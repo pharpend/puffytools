74835fd4447269348d7d8e8697656404a2ea6c6c
Peter Harpending <pharpend2@gmail.com>
2014-11-04 02:58:50 -0700

    Add slugs to mittens journals.
    Slugs are short, one-off pieces of text used to uniquely identify a journal.

--- lib/Mittens/Journal.hs
+++ lib/Mittens/Journal.hs
@@ -13,12 +13,18 @@ module Mittens.Journal where
 
 import           Control.Applicative
 import           Data.Aeson
+import           Data.Monoid
+import qualified Data.ByteString.Lazy as B
 import           Data.Text (Text)
 import           Data.Time
 import           Data.Vector (Vector)
+import           Paths_mittens
+import           Mittens.Slug
+import           System.IO
 
 -- |A Journal is really a wrapper around a list of entries
-data Journal = Journal { journalTitle :: Text
+data Journal = Journal { journalSlug :: Slug
+                       , journalTitle :: Text
                        , journalLastEdited :: UTCTime
                        , journalCreated :: UTCTime
                        , journalDescription :: Maybe Text
@@ -35,7 +41,8 @@ data Entry = Entry { entrySummary :: Text
   deriving (Show, Eq)
 
 instance FromJSON Journal where
-  parseJSON (Object v) = Journal <$> v .: "title"
+  parseJSON (Object v) = Journal <$> v .: "slug"
+                                 <*> v .: "title"
                                  <*> v .: "last-edited"
                                  <*> v .: "created"
                                  <*> v .: "description"
@@ -43,12 +50,13 @@ instance FromJSON Journal where
   parseJSON _ = fail "Must be an object"
 
 instance ToJSON Journal where
-  toJSON (Journal t le cr des ent) = object [ "title" .= t
-                                            , "last-edited" .= le
-                                            , "created" .= cr
-                                            , "description" .= des
-                                            , "entries" .= ent
-                                            ]
+  toJSON (Journal s t le cr des ent) = object [ "slug" .= s
+                                              , "title" .= t
+                                              , "last-edited" .= le
+                                              , "created" .= cr
+                                              , "description" .= des
+                                              , "entries" .= ent
+                                              ]
 
 instance FromJSON Entry where
   parseJSON (Object v) = Entry <$> v .: "summary"
@@ -63,3 +71,28 @@ instance ToJSON Entry where
                     , "last-edited" .= entryLastEdited e
                     , "description" .= entryDescription e
                     ]
+
+-- |Makes a "blank" journal
+mkJournal :: IO Journal
+mkJournal = Journal <$> mkRandomSlug
+                    <*> pure mempty
+                    <*> getCurrentTime
+                    <*> getCurrentTime
+                    <*> pure mempty
+                    <*> pure mempty
+
+-- |Figures out the file path for a journal
+generateJournalPath :: Journal -> IO FilePath
+generateJournalPath j = getDataDir >>= \d -> return $ d <> "/" <> unSlug (journalSlug j) <> ".json"
+
+-- |Writes a journal to the default file path (~/.mittens/journal-title.json)
+writeJournalDef ::  Journal -> IO ()
+writeJournalDef j = generateJournalPath j >>= \fp -> writeJournalToFile fp j 
+
+-- |Writes a journal to a file path
+writeJournalToFile :: FilePath -> Journal -> IO ()
+writeJournalToFile fp j = openFile fp WriteMode >>= \h -> writeJournalToHandle h j
+
+-- |Writes a journal to a handle
+writeJournalToHandle :: Handle -> Journal -> IO ()
+writeJournalToHandle h j = B.hPut h $ encode j
---
+++ lib/Mittens/Slug.hs
@@ -0,0 +1,76 @@
+{- |
+Module       : Mittens.Slug
+Description  : Generate slugs
+Copyright    : 2014, Peter Harpending
+License      : BSD3
+Maintainer   : Peter Harpending <pharpend2@gmail.com>
+Stability    : experimental
+Portability  : Linux
+
+These generate slugs (file names).
+
+The slug must be alphanumeric, with the exception of "-_". It also
+must be between 4 and 32 chars long
+-}
+
+module Mittens.Slug
+       (Slug, mkSlugMaybe, mkSlugEither, mkRandomSlug, slugAcceptChars,
+        unSlug)
+       where
+
+import           Control.Monad
+import           Data.Aeson
+import           Data.Monoid ((<>))
+import           Data.Random
+import           Data.Text (unpack)
+
+-- |Newtype wrapper for String
+newtype Slug = MkSlug { unSlug :: String }
+  deriving (Eq, Show)
+
+-- |Maybe make a Slug
+mkSlugMaybe :: String -> Maybe Slug
+mkSlugMaybe s = case mkSlugEither s of
+  Left _ -> Nothing
+  Right q -> Just q
+
+-- |Try to make a Slug, return an error if I can't
+mkSlugEither :: String -> Either String Slug
+mkSlugEither s
+  | length s < 4 =
+    Left "The slug must be at least 4 chars long."
+  | length s > 32 =
+    Left "The slug must be at most 32 chars long."
+  | ftb /= s =
+    Left $ "The slug may only contain these characters: " <> slugAcceptChars
+  | otherwise =
+    Right $ MkSlug s
+  where
+    -- ftb ~ "filter badness"
+    ftb :: String
+    ftb = filter (`elem` slugAcceptChars) s
+
+-- |Generates a random slug 32 chars long
+mkRandomSlug :: IO Slug
+mkRandomSlug = replicateM 32 ioc >>= \s -> case mkSlugEither s of
+                                       Left err -> fail err
+                                       Right slg -> return slg
+  where
+    ioc :: IO Char
+    ioc = runRVar rvc StdRandom
+
+    rvc :: RVar Char
+    rvc = randomElement slugAcceptChars
+
+-- |Acceptable characters for a slug
+slugAcceptChars :: String
+slugAcceptChars = ['A' .. 'Z'] <> ['a' .. 'z'] <> ['0' .. '9'] <> "-_"
+
+instance FromJSON Slug where
+  parseJSON (String s) = case mkSlugEither (unpack s) of
+    Left err -> fail err
+    Right slg -> return slg
+  parseJSON _ = fail "Slug must be of type String."
+
+instance ToJSON Slug where
+  toJSON (MkSlug s) = toJSON s
