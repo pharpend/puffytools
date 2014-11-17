b4e064a1e37525b9a5f3e2455d0afb17e82dfedd
Peter Harpending <pharpend2@gmail.com>
2014-11-17 16:54:35 -0700

    Add successful minimal usage of the console-program library.

--- ptk/Main.hs
+++ ptk/Main.hs
@@ -11,6 +11,39 @@ Portability  : Linux
 
 module Main where
 
-main :: IO ()
-main = return ()
+import           Data.Version
+import           Paths_puffytools
+import           System.Console.Argument
+import           System.Console.Command
+import           System.Console.Program
 
+versionTree :: Commands IO
+versionTree = Node commandVersion []
+  where
+    commandVersion = Command
+                       "version"
+                       "Print the version to the console. Meant for use in other programs."
+                       printVersion
+    printVersion = io . putStr $ showVersion version
+
+helpTree :: Commands IO
+helpTree =
+  let helpCommand = Command "help" "Show this help menu." help
+  in Node helpCommand []
+
+help :: Action IO
+help = io $ showUsage commandTree
+
+commandTree :: Commands IO
+commandTree = Node (Command "ptk" description help) [journalTree, helpTree, versionTree]
+  where
+    description = "The Puffy Toolkit, version " ++ showVersion version
+
+journalTree :: Commands IO
+journalTree = Node journalCommand []
+  where
+    journalCommand = Command "journal" "Do things with Journals" journalHelp
+    journalHelp = io $ showUsage journalTree
+
+main :: IO ()
+main = single commandTree
